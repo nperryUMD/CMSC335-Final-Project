3b567274d3b93d24ec73dcd97d82205938542a52
Nicholas Perry <nperry1215@gmail.com>
2022-12-15 13:29:03 -0500

    Added the ability for users to add a place

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ let http = require("http");
 let port_number = 5001;
 let path = require("path");
 let express = require("express");
+const {ObjectId} = require('mongodb');
 require("dotenv").config({ path: path.resolve(__dirname, '.env') })  
 
 const userName = process.env.MONGO_DB_USERNAME;
@@ -26,11 +27,20 @@ async function insertPlace(client, databaseAndCollection, place) {
     const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(place);
 }
 
+async function getById(client, databaseAndCollection, id) {
+    let filter = {_id : ObjectId(id)};
+    const result = client.db(databaseAndCollection.db)
+    .collection(databaseAndCollection.collection)
+    .findOne(filter);
+
+    return result;
+}
+
 async function getAll(client, databaseAndCollection) {
     let filter = {};
     const cursor = client.db(databaseAndCollection.db)
     .collection(databaseAndCollection.collection)
-    .find(filter);;
+    .find(filter);
 
     const result = await cursor.toArray();
     return result;
@@ -51,7 +61,7 @@ app.get("/search", async (request, response) => {
 
     try {
         await client.connect();
-                places = await getAll(client, databaseAndCollection);
+        places = await getAll(client, databaseAndCollection);
     } catch (e) {
         console.error(e);
     } finally {
@@ -59,7 +69,7 @@ app.get("/search", async (request, response) => {
     }
     
     for(var i = 0; i < places.length; i++){
-        results += "<div class='results'><image class='resultImages' src=" + places[i].image + "></image><button class='rentButton' onclick='rent(" + places[i].id + ")'>Rent Me!</button><p>Address: " 
+        results += "<div class='results'><image class='resultImages' src=" + places[i].image + "></image><button class='rentButton' onclick=\"rent('" + places[i]._id.toString() + "')\">Rent Me!</button><p>Address: " 
                     + places[i].street + ", " + places[i].city + " " + places[i].state + " " 
                     + places[i].zip + "</p><p>Price: $" + places[i].cost + "/night</p><p>Owner: " + places[i].owner + "</p></div>";
     }
@@ -71,18 +81,59 @@ app.get("/rentOutPlace", (request, response) =>{
     response.render("rentOutPlace.ejs");
 });
 
-app.get("/rentPlace", (request, response) =>{
-    response.render("rentPlace.ejs");
+app.get("/rentPlace", async (request, response) =>{
+    var id = request.query.id;
+    if(id == undefined || id == null || id.length == 0){
+        location = "";
+        response.redirect("/search");
+    }
+    var place;
+
+    try {
+        await client.connect();
+        place = await getById(client, databaseAndCollection, id);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+
+    if(!place){
+        response.redirect("/search");
+    }
+
+    var addy = place.street + ", " + place.city + " " + place.state + " " + place.zip;
+    response.render("rentPlace.ejs", {address: addy, cost: place.cost, owner: place.owner, image: place.image});
+});
+
+app.post("/rentOutPlace", async (request, response) =>{
+    var name = request.body.name;
+    var street = request.body.street;
+    var city = request.body.city;
+    var state = request.body.state;
+    var zip = request.body.zip;
+    var cost = request.body.cost;
+    var image = request.body.image;
+    var place = {owner: name, street: street, city: city, state: state, zip: zip, cost: cost, image: image};
+
+    try {
+        await client.connect();
+        await insertPlace(client, databaseAndCollection, place);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
 });
 
 
 app.post("/loadFakeData", async (request, response) =>{
-    var places = [{id: "1", owner: "Nicholas Perry", street: "8204 Baltimore ave", city: "College Park", state: "Maryland", zip: "20740", cost: "50", image: "https://odis.homeaway.com/odis/destination/5941b1e0-2600-4b2a-b27e-c667abf7e510.carousel-m.jpg"}, 
-                    {id: "2", owner: "asdf asdfadsf", street: "232 vasedfd ave", city: "College Park", state: "Maryland", zip: "20740", cost: "80", image: "https://i0.wp.com/files.tripstodiscover.com/files/2021/07/Bamboo-House-Netflix.jpg?resize=784%2C521"},
-                    {id: "3", owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100" ,image: "https://www.southernliving.com/thmb/eOuNUEM15ZXO2hDAK7oXVVR17Fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gettyimages-525981286-2000-dac41913bef747e78a797b341dc3708d.jpg"},
-                    {id: "4", owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://images.ctfassets.net/gxwgulxyxxy1/7Dhves120IEQzq3Zz7yOo6/64f12e4d88f4ac268f6d842dcf807aa9/lake-1453079_1280.jpg"},
-                    {id: "5", owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://www.fortunebuilders.com/wp-content/uploads/2021/04/best-places-to-buy-vacation-rental-property.jpg"},
-                    {id: "6", owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://wp-tid.zillowstatic.com/18/FL_TPA_HISTORIC_KENWOOD_82561_239_qrt-d7969b.jpg"}];
+    var places = [{owner: "Nicholas Perry", street: "8204 Baltimore ave", city: "College Park", state: "Maryland", zip: "20740", cost: "50", image: "https://odis.homeaway.com/odis/destination/5941b1e0-2600-4b2a-b27e-c667abf7e510.carousel-m.jpg"}, 
+                    {owner: "asdf asdfadsf", street: "232 vasedfd ave", city: "College Park", state: "Maryland", zip: "20740", cost: "80", image: "https://i0.wp.com/files.tripstodiscover.com/files/2021/07/Bamboo-House-Netflix.jpg?resize=784%2C521"},
+                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100" ,image: "https://www.southernliving.com/thmb/eOuNUEM15ZXO2hDAK7oXVVR17Fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gettyimages-525981286-2000-dac41913bef747e78a797b341dc3708d.jpg"},
+                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://images.ctfassets.net/gxwgulxyxxy1/7Dhves120IEQzq3Zz7yOo6/64f12e4d88f4ac268f6d842dcf807aa9/lake-1453079_1280.jpg"},
+                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://www.fortunebuilders.com/wp-content/uploads/2021/04/best-places-to-buy-vacation-rental-property.jpg"},
+                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", image: "https://wp-tid.zillowstatic.com/18/FL_TPA_HISTORIC_KENWOOD_82561_239_qrt-d7969b.jpg"}];
     
     try {
         await client.connect();
