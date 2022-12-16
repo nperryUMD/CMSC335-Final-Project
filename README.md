06bed6cd245e03f39ea8dd0cf438ed53979616ea
Amoni Hawkins <amonihawkins@wireless-10-104-100-227.umd.edu>
2022-12-16 12:18:06 -0500

    more updates

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # CMSC335-Final-Project
+
+Team Members: 
+Nicholas Perry (directory id) and Amoni Hawkins (ahawkin8)
+
+App Description: 
+Welcome to Not AirBnB :)! This website allows you to browse through some home listings in both
+Maryland and Virgina. You can search for specific listings based on the cities of College Park, MD 
+and McLean, VA, rent out a space or remove a space.
+
+API Links:
+
+
+Youtube Video Link:
--- server.js
+++ server.js
@@ -289,24 +289,24 @@ app.post("/removeProperty", async (request, response) =>{
 
 
 app.post("/loadFakeData", async (request, response) =>{
-    var places = [{owner: "Nicholas Perry", street: "8204 Baltimore ave", city: "College Park", state: "Maryland", zip: "20740", 
-                    cost: "50", bed: "3", bath: "2", image: "https://odis.homeaway.com/odis/destination/5941b1e0-2600-4b2a-b27e-c667abf7e510.carousel-m.jpg", availablility: true}, 
+    var places = [{owner: "Nicholas Perry", street: "8204 Baltimore Ave", city: "College Park", state: "Maryland", zip: "20740", 
+                    cost: "50", bed: "3", bath: "2.5", image: "https://odis.homeaway.com/odis/destination/5941b1e0-2600-4b2a-b27e-c667abf7e510.carousel-m.jpg", availablility: true}, 
                     
-                    {owner: "asdf asdfadsf", street: "232 vasedfd ave", city: "College Park", state: "Maryland", zip: "20740", 
+                    {owner: "Amoni Hawkins", street: "232 vasedfd ave", city: "College Park", state: "Maryland", zip: "20740", 
                     cost: "80", bed: "3", bath: "2", image: "https://i0.wp.com/files.tripstodiscover.com/files/2021/07/Bamboo-House-Netflix.jpg?resize=784%2C521", availablility: true},
                     
-                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", cost: "100", bed: "3", bath: "2", 
+                    {owner: "Elon Musk", street: "1850 Patton Ter", city: "McLean", state: "Virginia", zip: "22101", cost: "100", bed: "3", bath: "3", 
                     image: "https://www.southernliving.com/thmb/eOuNUEM15ZXO2hDAK7oXVVR17Fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/gettyimages-525981286-2000-dac41913bef747e78a797b341dc3708d.jpg", 
                     availablility: true},
                     
-                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", 
-                    cost: "100", bed: "3", bath: "2", image: "https://images.ctfassets.net/gxwgulxyxxy1/7Dhves120IEQzq3Zz7yOo6/64f12e4d88f4ac268f6d842dcf807aa9/lake-1453079_1280.jpg", availablility: true},
+                    {owner: "Nelson Paldua-Perez", street: "8000 Old Falls Rd", city: "McLean", state: "Virginia", zip: "22102", 
+                    cost: "150", bed: "4", bath: "4", image: "https://images.ctfassets.net/gxwgulxyxxy1/7Dhves120IEQzq3Zz7yOo6/64f12e4d88f4ac268f6d842dcf807aa9/lake-1453079_1280.jpg", availablility: true},
                     
-                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", 
-                    cost: "100", bed: "3", bath: "2", image: "https://www.fortunebuilders.com/wp-content/uploads/2021/04/best-places-to-buy-vacation-rental-property.jpg", availablility: true},
+                    {owner: "Harry Styles", street: "8636 Old Dominion Dr", city: "McLean", state: "Virginia", zip: "22102", 
+                    cost: "120", bed: "3", bath: "2.5", image: "https://www.fortunebuilders.com/wp-content/uploads/2021/04/best-places-to-buy-vacation-rental-property.jpg", availablility: true},
                     
-                    {owner: "scooby do", street: "705 church st", city: "sipper bark", state: "Virginia", zip: "20368", 
-                    cost: "100", bed: "3", bath: "2", image: "https://wp-tid.zillowstatic.com/18/FL_TPA_HISTORIC_KENWOOD_82561_239_qrt-d7969b.jpg", availablility: true}];
+                    {owner: "Rihanna Fenty", street: "4118 Stadium Dr", city: "College Park", state: "Maryland", zip: "20740", 
+                    cost: "70", bed: "2", bath: "1", image: "https://wp-tid.zillowstatic.com/18/FL_TPA_HISTORIC_KENWOOD_82561_239_qrt-d7969b.jpg", availablility: true}];
     
     try {
         await client.connect();
@@ -324,7 +324,7 @@ app.post("/loadFakeData", async (request, response) =>{
 
 
 http.createServer(app).listen(port_number);
-console.log(`Web server started and running on port: ${port_number}`);
+console.log(`Web server started and running on: http://localhost:${port_number}`);
 process.stdout.write("Stop to shutdown the server: ");
 
 process.stdin.on('data', (data) => {
